d3723463748ff1197d60e35112f05ad0176dfb21
Ahmad Tanwir <ata@javan.co.id>
2010-01-29 15:11:43 +0700

    Goal Adding

--- goal/Goal/GuestPage.php
+++ goal/Goal/GuestPage.php
@@ -1,13 +1,55 @@
 <?php
 namespace Goal;
 
-class GuestForm extends BasePage
+use Ostric\Component\Markup\Panel;
+use Ostric\Component\Markup\Form;
+use Ostric\Component\Markup\Form\InputText;
+use Ostric\Component\Markup\Form\TextArea;
+use Ostric\Component\Markup\Container;
+use Ostric\Component\DataList;
+
+class Guest{
+    
+    public $name;
+    public $email;
+    public $comment;
+}
+
+class GuestForm extends Form
 {
-    public $title = 'Guest Page';
-    public $form = null;
+    
+    public function __construct($id, $guest)
+    {
+        parent::__construct($id, $guest);
+        $this->add(new InputText('name'));
+        $this->add(new InputText('email'));
+        $this->add(new TextArea('comment'));
+        
+    }
+    
+    public function onSubmit()
+    {
+        array_push(GuestPage::$guests,$this->guest);
+    }
+}
+
+class GuestPage extends BasePage
+{
+    public static $guests = array();
     
     public function __construct()
     {
-        $this->form = new GuestBookForm();
+        $this->add(new GuestBookForm('form', new Guest()));
+        
+        $posts = new DataList('posts');
+        $this->add($posts);
+        
+        // entry data for list item
+        foreach(self::$guests as $guest)
+        {
+            $posts->addItem(new Label('name',$guest->name));
+            $posts->addItem(new Label('comment',$guest->comment));
+        }
+        
     }
 }
--- goal/Goal/SideBarPanel.php
+++ goal/Goal/SideBarPanel.php
@@ -2,25 +2,29 @@
 namespace Goal;
 
 use Ostric\Component\Markup\Panel;
+use Ostric\Component\Markup\Link;
 use Ostric\Component\DataList;
-
-class SideMenuList extends DataList{
-    protected function each($key, $value)
-    {
-        $menulink = new Link('menulink',$value);
-        $this->add($menulink);
-        $menulink->add(new Label('menulabel', $key));
-    }
-}
 
 class SideBarPanel extends Panel
 {
     
-    public function __construct()
+    public function __construct($id)
     {
+        parent::__construct($id);
+        
+        $sidemenus = new DataList('sidemenus');
+        $this->add($sidemenus);
+        // example data for datalist
         $urls = array(  'google' => 'http://google.com',
                         'yahoo' => 'http://yahoo.com'
                         'facebook' => 'http://facebook.com');
-        $this->add(new SideMenuList('sidemenu',$urls));
+        
+        // entry data for list item
+        foreach($urls as $label => $url)
+        {
+            $menulink = new Link('menulink',$url);
+            $sidemenus->addItem($menulink);
+            $menulink->add(new Label('menulabel'));
+        }
     }
 }
